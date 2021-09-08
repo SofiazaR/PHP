6878e3166cb64b8cb26d849342326f1767b1e78d
Sofiana <razuvalovasofa@gmail.com>
2021-09-08 11:53:10 +0300

    Initial commit

---
+++ main.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ru">
+<meta charset="utf-8"/>
+<head><title>Проект 42</title></head>
+<body>
+<?php
+$message = "Hello";
+?>
+<h1><?=$message?></h1>
+</body>
+</html>
+
+
